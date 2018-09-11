6cf0df5f5f05fc6f97b380b31d143c0bbd1ac7bd
Pablo Jabat <pablojabat@gmail.com>
2018-09-11 14:13:08 +0200

    minor changes to main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import sys
 import random
 import pickle
 
-print 'Number of arguments:', len(sys.argv), 'arguments.'
-print 'Argument List:', str(sys.argv)
+print('Number of arguments:', len(sys.argv), 'arguments.')
+print('Argument List:', str(sys.argv))
 
 if __name__ == "__main__":
 
@@ -26,6 +26,6 @@ if __name__ == "__main__":
 
 	weights_matrix, word2idx = dp.get_weight_matrix(tokens, word2vector, dims, True)
 
-	sentence_matrices, labels_matrices = dp.process_dataset(path, word2idx, labels, read = False)
+	sentence_matrices, labels_matrices = dp.process_dataset(path, word2idx, labels)
 
 	print("Program executed succesfully ...")
