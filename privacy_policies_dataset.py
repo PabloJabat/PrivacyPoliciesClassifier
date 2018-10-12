7d3fa9c84d328912c216daf80fe7d72cb906bc3c
Pablo Jabat <pablojabat@gmail.com>
2018-10-12 16:27:57 +0200

    added new PrivacyPolicyDataset class for testing

--- privacy_policies_dataset.py
+++ privacy_policies_dataset.py
@@ -1,6 +1,11 @@
 import data_processing as dp
+
 import numpy as np
+
 import torch
+
+from torch import tensor
+
 from torch.utils.data import Dataset
 
 class PrivacyPoliciesDataset(Dataset):  
@@ -139,4 +144,92 @@ class PrivacyPoliciesDataset(Dataset):
             self.segments_list = torch.stack(self.segments_list)
             
             self.labels_list = torch.stack(self.labels_list)
-                   
+
+class PrivacyPoliciesDataset_all(Dataset):
+    
+    def __init__(self, segments_array, labels_list, labels):
+        
+        self.segments_array = segments_array
+       
+        self.labels_tensor = tensor(labels_list)
+        
+        self.labels = labels
+        
+    def __len__(self):
+        
+        if self.segments_array.shape[0] == self.labels_tensor.shape[0]: 
+            
+            return self.segments_array.shape[0]
+        
+        else:
+            
+            print("Warning: number of segments don't match number of annotations")
+            
+            return self.segments_array.shape[0]     
+    
+    def __getitem__(self, idx):
+        
+        segment = self.segments_array[idx]
+        
+        label = self.labels_tensor[idx]
+        
+        return (segment, label)
+    
+    def split_dataset_randomly(self, ratio = 0.1):
+    
+        """
+
+        This function randomly splits the dataset in two parts using the split ratio provided
+
+        Args:
+            dataset: torch.utils.data.Dataset, dataset containing the data to split
+            ratio: double, percentage of data that will be retrieved inside s_dataset
+        Returns:
+            s_dataset: torch.utils.data.Dataset, dataset with length = len(dataset) * ratio
+            b_dataset: torch.utils.data.Dataset, dataset with length = len(dataset) * (1 - ratio)
+
+        """
+
+        from random import sample
+
+        labels = self.labels
+
+        num_samples = int(ratio * len(self))
+
+        s_dataset_idx_set = set(sample(range(len(self)), num_samples))
+
+        b_dataset_idx_set = set(range(len(self))).difference(s_dataset_idx_set)
+
+        s_dataset_idx_tensor = tensor(list(s_dataset_idx_set))
+
+        b_dataset_idx_tensor = tensor(list(b_dataset_idx_set))
+
+        s_dataset_data = self[s_dataset_idx_tensor]
+
+        b_dataset_data = self[b_dataset_idx_tensor]
+
+        s_dataset = PrivacyPoliciesDataset_all(s_dataset_data[0], s_dataset_data[1], labels)
+
+        b_dataset = PrivacyPoliciesDataset_all(b_dataset_data[0], b_dataset_data[1], labels)
+
+        return s_dataset, b_dataset
+    
+    def pickle_dataset(self, path):
+
+        import pickle
+
+        with open(path, "wb") as dataset_file:
+
+            pickle.dump(self, dataset_file)
+    
+    @staticmethod
+    def unpickle_dataset(path):
+    
+        import pickle
+
+        with open(path, "rb") as dataset_file:
+
+            dataset = pickle.load(dataset_file)
+
+            return dataset
+    
