7456e14dc0f35c6736d539efc168f52d898d6843
Pablo Jabat <pablojabat@gmail.com>
2018-10-16 12:38:24 +0200

    reorganized cnn.py

--- cnn.py
+++ cnn.py
@@ -12,6 +12,10 @@ from privacy_policies_dataset import PrivacyPoliciesDataset_all as PPD
 
 import sys
 
+import numpy as np
+
+import matplotlib.pyplot as plt
+
 import time
 
 import pickle
@@ -115,160 +119,307 @@ class CNN(nn.Module):
         output_file = open(self.cnn_name + "_params.pkl", "wb")
         
         pickle.dump(cnn_params, output_file)
-
-def train_cnn(model, dataset, lr = 0.02, epochs_num = 100, batch_size = 40, momentum = 0.9):
-    """
-    
-    This function trains a CNN model using gradient descent with the posibility of using momentum. 
-    
-    Args:
-        model: cnn.CNN, an instance of a model of the class cnn.CNN 
-        train_dataloader: Dataloader, Dataloader instance built using PrivacyPoliciesDataset instance
-        lr: double, learning rate that we want to use in the learning algorithm
-        epochs_num: integer, number of epochs
-        momentum: double, momentum paramenter that tunes the momentum gradient descent algorithm    
-    Returns:
-        epochs: list, list containing all the epochs
-        losses: list, list containing the loss at the beginning of each epoch
-    
-    """
-
-    optimizer = SGD(model.parameters(), lr = lr, momentum = momentum)
-
-    criterion = nn.BCELoss()
-
-    train_losses = []
-
-    validation_losses = []
-    
-    epochs = []
-
-    start = time.time()
-
-    remaining_time = 0
-
-    for epoch in range(epochs_num):
         
-        validation_dataset, train_dataset = dataset.split_dataset_randomly()
+    def train_cnn(self, dataset, lr = 0.02, epochs_num = 100, batch_size = 40, momentum = 0.9):
+        """
+
+        This function trains a CNN model using gradient descent with the posibility of using momentum. 
+
+        Args:
+            model: cnn.CNN, an instance of a model of the class cnn.CNN 
+            train_dataloader: Dataloader, Dataloader instance built using PrivacyPoliciesDataset instance
+            lr: double, learning rate that we want to use in the learning algorithm
+            epochs_num: integer, number of epochs
+            momentum: double, momentum paramenter that tunes the momentum gradient descent algorithm    
+        Returns:
+            epochs: list, list containing all the epochs
+            losses: list, list containing the loss at the beginning of each epoch
+
+        """
+
+        optimizer = SGD(self.parameters(), lr = lr, momentum = momentum)
+
+        criterion = nn.BCELoss()
+
+        train_losses = []
+
+        validation_losses = []
+
+        epochs = []
+
+        start = time.time()
+
+        remaining_time = 0
+
+        for epoch in range(epochs_num):
+
+            validation_dataset, train_dataset = dataset.split_dataset_randomly()
+
+            train_dataloader = DataLoader(train_dataset, batch_size = batch_size, collate_fn = PPD.collate_data)
+
+            for i_batch, sample_batched in enumerate(train_dataloader):
+
+                input = sample_batched[0]
+
+                target = sample_batched[1].float()
+
+                self.zero_grad()
+
+                output = self(input)
+
+                train_loss = criterion(output, target)
+
+                train_loss.backward()
+
+                optimizer.step()
+
+            validation_segments, validation_labels = PPD.collate_data(validation_dataset)
+
+            validation_loss = criterion(self(validation_segments.long()), validation_labels.float())
+
+            end = time.time()
+
+            remaining_time = remaining_time * 0.90 + ((end - start) * (epochs_num - epoch + 1) / (epoch + 1)) * 0.1
+
+            remaining_time_corrected = remaining_time / (1 - (0.9 ** (epoch + 1)))
+
+            epoch_str = "last epoch finished: " + str(epoch)
+
+            progress_str = "progress: " + str((epoch + 1) * 100 / epochs_num) + "%"
+
+            time_str = "time: " + str(remaining_time_corrected / 60) + " mins"
+
+            sys.stdout.write("\r" + epoch_str + " -- " + progress_str + " -- " + time_str)
+
+            sys.stdout.flush()
+
+            train_losses.append(train_loss.item())
+
+            validation_losses.append(validation_loss.item())
+
+            epochs.append(epoch)
+
+        print("\n" + "Training completed. Total training time: " + str(round((end - start) / 60, 2)) + " mins")
+
+        return epochs, train_losses, validation_losses
+    
+    def print_results(self, train_dataset, test_dataset, labels):
+
+        y_train = train_dataset.labels_tensor
+
+        y_test = test_dataset.labels_tensor
+
+        x_train = PPD.collate_data(train_dataset)[0]
+
+        x_test = PPD.collate_data(test_dataset)[0]
+
+        y_hat_train = self(x_train)
+
+        y_hat_test = self(x_test)
+
+        # This will be the x axis
+        threshold_list = np.arange(0.0, 1, 0.01)
+
+        # These will be the y axis data
+        f1_scores_test = [self.f1_score(y_test, y_hat_test, t)[0] for t in threshold_list]
+
+        precisions_test = [self.f1_score(y_test, y_hat_test, t)[1] for t in threshold_list]
+
+        recalls_test = [self.f1_score(y_test, y_hat_test, t)[2] for t in threshold_list]
+
+        f1_scores_train = [self.f1_score(y_train, y_hat_train, t)[0] for t in threshold_list]
+
+        precisions_train =[self.f1_score(y_train, y_hat_train, t)[1] for t in threshold_list]
+
+        recalls_train = [self.f1_score(y_train, y_hat_train, t)[2] for t in threshold_list]
+
+        """
+        Here comes the pyplot code
+        """
+
+        fig = plt.figure(figsize=(15,4))
+
+        # We start with the three pyplot axis we want. One for F1, another for precision and one last one for recall
+        ax_f1 = fig.add_subplot(131)
+
+        ax_precision = fig.add_subplot(132)
+
+        ax_recall = fig.add_subplot(133)
+
+        # We now plot all the data in te corresponding axis
+        ax_f1.plot(threshold_list, f1_scores_test, label='test')
+
+        ax_f1.plot(threshold_list, f1_scores_train, label='train')
+
+        ax_f1.set_title('F1 Score vs Threshold')
+
+        ax_f1.set_ylim(0,1.05)
+
+        ax_f1.legend()
+
+        ax_precision.plot(threshold_list, precisions_test, label='test')
+
+        ax_precision.plot(threshold_list, precisions_train, label='train')
+
+        ax_precision.set_title('Precision vs Threshold')
+
+        ax_precision.set_ylim(0,1.05)
+
+        ax_precision.legend()
+
+        ax_recall.plot(threshold_list, recalls_test, label='test')
+
+        ax_recall.plot(threshold_list, recalls_train, label='train')
+
+        ax_recall.set_title('Recall vs Threshold')
+
+        ax_recall.set_ylim(0,1.05)
+
+        ax_recall.legend()
+
+        plt.show()
+
+        # We show the F1, precision and recall for a threshold of 0.5
+        f1, precision, recall = self.f1_score(y_test, y_hat_test, 0.5)
+
+        print("Scores with 0.5 threshold")
+
+        print("-" * 35 * 3)
+
+        print("f1        |" + str(f1))
+
+        print("precision |" + str(precision))
+
+        print("recall    |" + str(recall))
+
+        print("-" * 35 * 3)
+
+        best_f1_label, best_t_label = self.get_best_thresholds(y_test, y_hat_test, labels)
+
+        print("\n" + "F1 Score per Label")
+
+        print("-" * 35 * 3)
+
+        row_format ="{:<38}" * 3
+
+        print(row_format.format("Label", "F1", "Threshold"))
+
+        print("-" * 35 * 3)
+
+        for label, index in labels.iteritems():
+
+            print row_format.format(label, best_f1_label[index], best_t_label[index])
+
+        # We save the figure into a picture
+        fig.savefig(fname = self.cnn_name + '.png', format = 'png')
+
+    @staticmethod
+    def get_best_thresholds(y_test, y_hat_test, labels):
         
-        train_dataloader = DataLoader(train_dataset, batch_size = batch_size, collate_fn = PPD.collate_data)
-        
-        for i_batch, sample_batched in enumerate(train_dataloader):
+        threshold_list = np.arange(0.0, 1, 0.01)
 
-            input = sample_batched[0]
+        best_f1_label = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-            target = sample_batched[1].float()
+        best_t_label = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-            model.zero_grad()
+        for label, index in labels.items():
 
-            output = model(input)
+            best_f1 = 0
 
-            train_loss = criterion(output, target)
+            best_t = 0
 
-            train_loss.backward()
+            for t in threshold_list:
 
-            optimizer.step()
-            
-        validation_segments, validation_labels = PPD.collate_data(validation_dataset)
-            
-        validation_loss = criterion(model(validation_segments.long()), validation_labels.float())
+                current_f1 = CNN.f1_score_per_label(y_test, y_hat_test, t)[0][labels[label]].item()
 
-        end = time.time()
+                if current_f1 > best_f1: 
 
-        remaining_time = remaining_time * 0.90 + ((end - start) * (epochs_num - epoch + 1) / (epoch + 1)) * 0.1
+                    best_f1 = current_f1
 
-        remaining_time_corrected = remaining_time / (1 - (0.9 ** (epoch + 1)))
+                    best_t = t
 
-        epoch_str = "last epoch finished: " + str(epoch)
+            best_f1_label[index] = best_f1
 
-        progress_str = "progress: " + str((epoch + 1) * 100 / epochs_num) + "%"
+            best_t_label[index] = best_t
 
-        time_str = "time: " + str(remaining_time_corrected / 60) + " mins"
+        return best_f1_label, best_t_label
+              
+    @staticmethod    
+    def f1_score(y_true, y_pred, threshold, dim = 0, eps = 1e-9):
+        """
 
-        sys.stdout.write("\r" + epoch_str + " -- " + progress_str + " -- " + time_str)
+        Computes the f1 score resulting from the comparison between y_true and y_pred after using the threshold set.
 
-        sys.stdout.flush()
+        Args: 
+            y_true: torch.tensor, 2-dimensional torch.tensor containing the true labels per record. The i-th row is the 
+            record i whereas the j-th column is the label j.
+            y_pred: torch.tensor, 2-dimensional torch.tensor containing the probabilities assigned to each label. The i-th 
+            row is the record i whereas the j-th column is the label j.
+            threshold: double, number between 0 and 1 that sets the threshold probability for a label to be truly assigned to 
+            a record.
+            dim: int, it has to be either 0 or 1. It is dimension in which we sum the data. We do not recommend the user to set 
+            this parameter.
+            eps: double, it is just a very small value that avoids dividing by 0 when computing the precision and recall. 
 
-        train_losses.append(train_loss.item())
-        
-        validation_losses.append(validation_loss.item())
+        Returns:
+            f1: double, the resulting mean f1 score of all the labels (it will be a number between 0 and 1)
+            precision: double, the resulting mean precision of all the labels (it will be a number between 0 and 1)
+            recall: double, the resulting mean recall of all the labels (it will be a number between 0 and 1)
 
-        epochs.append(epoch)
+        """
 
-    print("\n" + "Training completed. Total training time: " + str(round((end - start) / 60, 2)) + " mins")
+        y_pred = torch.ge(y_pred.float(), threshold).float()
+
+        y_true = y_true.float()
+
+        y_pred = torch.ge(y_pred.float(), threshold).float()
+
+        y_true = y_true.float()
+
+        true_positive = (y_pred * y_true).sum(dim = dim)
+
+        precision = true_positive.div(y_pred.sum(dim = dim).add(eps))
+
+        recall = true_positive.div(y_true.sum(dim = dim).add(eps))
+
+        f1 = torch.mean((precision * recall).div(precision + recall + eps) * 2)
+
+        return f1.item(), torch.mean(precision).item(), torch.mean(recall).item()
     
-    return epochs, train_losses, validation_losses
+    @staticmethod
+    def f1_score_per_label(y_true, y_pred, threshold, dim=0, eps=1e-9):
+        """
 
-def f1_score(y_true, y_pred, threshold, dim = 0, eps = 1e-9):
-    """
-    
-    Computes the f1 score resulting from the comparison between y_true and y_pred after using the threshold set.
-    
-    Args: 
-        y_true: torch.tensor, 2-dimensional torch.tensor containing the true labels per record. The i-th row is the record i
-        whereas the j-th column is the label j.
-        y_pred: torch.tensor, 2-dimensional torch.tensor containing the probabilities assigned to each label. The i-th row is
-        the record i whereas the j-th column is the label j.
-        threshold: double, number between 0 and 1 that sets the threshold probability for a label to be truly assigned to 
-        a record.
-        dim: int, it has to be either 0 or 1. It is dimension in which we sum the data. We do not recommend the user to set 
-        this parameter.
-        eps: double, it is just a very small value that avoids dividing by 0 when computing the precision and recall. 
-        
-    Returns:
-        f1: double, the resulting mean f1 score of all the labels (it will be a number between 0 and 1)
-        precision: double, the resulting mean precision of all the labels (it will be a number between 0 and 1)
-        recall: double, the resulting mean recall of all the labels (it will be a number between 0 and 1)
-    
-    """
-    
-    y_pred = torch.ge(y_pred.float(), threshold).float()
-    
-    y_true = y_true.float()
+        Computes the f1 score per label resulting from the comparison between y_true and y_pred after using the threshold set.
 
-    true_positive = (y_pred * y_true).sum(dim = dim)
-    
-    precision = true_positive.div(y_pred.sum(dim = dim).add(eps))
-    
-    recall = true_positive.div(y_true.sum(dim = dim).add(eps))
-    
-    f1 = torch.mean((precision * recall).div(precision + recall + eps) * 2)
+        Args: 
+            y_true: torch.tensor, 2-dimensional torch.tensor containing the true labels per record. The i-th row is the 
+            record i whereas the j-th column is the label j.
+            y_pred: torch.tensor, 2-dimensional torch.tensor containing the probabilities assigned to each label. The i-th 
+            row is the record i whereas the j-th column is the label j.
+            threshold: double, number between 0 and 1 that sets the threshold probability for a label to be truly assigned to 
+            a record.
+            dim: int, it has to be either 0 or 1. It is dimension in which we sum the data. We do not recommend the user to set 
+            this parameter.
+            eps: double, it is just a very small value that avoids dividing by 0 when computing the precision and recall.
 
-    return f1.item(), torch.mean(precision).item(), torch.mean(recall).item()
+        Returns:
+            f1: list, the resulting f1 score per label (it will be a number between 0 and 1)
+            precision: list, the resulting precision per label (it will be a number between 0 and 1)
+            recall: list, the resulting recall per label (it will be a number between 0 and 1)
 
-def f1_score_per_label(y_true, y_pred, threshold, dim=0, eps=1e-9):
-    """
-    
-    Computes the f1 score per label resulting from the comparison between y_true and y_pred after using the threshold set.
-    
-    Args: 
-        y_true: torch.tensor, 2-dimensional torch.tensor containing the true labels per record. The i-th row is the record i
-        whereas the j-th column is the label j.
-        y_pred: torch.tensor, 2-dimensional torch.tensor containing the probabilities assigned to each label. The i-th row is
-        the record i whereas the j-th column is the label j.
-        threshold: double, number between 0 and 1 that sets the threshold probability for a label to be truly assigned to 
-        a record.
-        dim: int, it has to be either 0 or 1. It is dimension in which we sum the data. We do not recommend the user to set 
-        this parameter.
-        eps: double, it is just a very small value that avoids dividing by 0 when computing the precision and recall.
-    
-    Returns:
-        f1: list, the resulting f1 score per label (it will be a number between 0 and 1)
-        precision: list, the resulting precision per label (it will be a number between 0 and 1)
-        recall: list, the resulting recall per label (it will be a number between 0 and 1)
-        
-    """
+        """
 
-    y_pred = torch.ge(y_pred.float(), threshold).float()
-    
-    y_true = y_true.float()
+        y_pred = torch.ge(y_pred.float(), threshold).float()
 
-    true_positive = (y_pred * y_true).sum(dim=dim)
-    
-    precision = true_positive.div(y_pred.sum(dim=dim).add(eps))
-    
-    recall = true_positive.div(y_true.sum(dim=dim).add(eps))
-    
-    f1 = (precision * recall).div(precision + recall + eps) * 2
+        y_true = y_true.float()
 
-    return f1, precision, recall
+        true_positive = (y_pred * y_true).sum(dim=dim)
+
+        precision = true_positive.div(y_pred.sum(dim=dim).add(eps))
+
+        recall = true_positive.div(y_true.sum(dim=dim).add(eps))
+
+        f1 = (precision * recall).div(precision + recall + eps) * 2
+
+        return f1, precision, recall
