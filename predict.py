2740ef4949e39011da76f137c784dd83a4dd3198
Pablo Jabat <pablojabat@gmail.com>
2018-11-28 11:24:49 +0100

    fixed indentations

--- predict.py
+++ predict.py
@@ -7,55 +7,55 @@ from privacy_policies_dataset import PrivacyPoliciesDataset as PPD
 
 def load_model(path, label):
     
-    #We make the imports needed for this function
-    import pickle
-    import torch
-    from os.path import join
-    
-    #We set the name of the model and its parameters
-    models_files = join(path, 'cnn_300_200_[100, 25]_1_[3]_label{}_polisis_state.pt')
-    model_file = models_files.format(label)
-    params_files = join(path, 'cnn_300_200_[100, 25]_1_[3]_label{}_polisis_params.pkl')
-    params_file = params_files.format(label)
-    
-    #We now load the parameters
-    with open(params_file, 'rb') as f:
-        params = pickle.load(f)
-        
-    #We now load the model and pass the parameters
-    model = CNN(**params)
-    model.load_state_dict(torch.load(model_file))
-    
-    return model
+	#We make the imports needed for this function
+	import pickle
+	import torch
+	from os.path import join
+
+	#We set the name of the model and its parameters
+	models_files = join(path, 'cnn_300_200_[100, 25]_1_[3]_label{}_polisis_state.pt')
+	model_file = models_files.format(label)
+	params_files = join(path, 'cnn_300_200_[100, 25]_1_[3]_label{}_polisis_params.pkl')
+	params_file = params_files.format(label)
+
+	#We now load the parameters
+	with open(params_file, 'rb') as f:
+		params = pickle.load(f)
+		
+	#We now load the model and pass the parameters
+	model = CNN(**params)
+	model.load_state_dict(torch.load(model_file))
+
+	return model
 
 def load_12CNN_model(path):
     
-    from collections import OrderedDict
-    #We instantiate an empty dictionary that will contain the models
-    model12cnn = OrderedDict()
-    for label in range(12):
-        model12cnn['model{}'.format(label)] = load_model(path, label)
-        
-    return model12cnn
+	from collections import OrderedDict
+	#We instantiate an empty dictionary that will contain the models
+	model12cnn = OrderedDict()
+	for label in range(12):
+		model12cnn['model{}'.format(label)] = load_model(path, label)
+		
+	return model12cnn
 
 def predict(data, models, threshold=0.5):
     
-    #We import time to compute time of prediction
-    import time
-    import torch
-    
-    x = PPD.collate_data(data)[0]
-    y = torch.tensor([])
-    
-    start = time.time()
-    for key, model in models.items():
-        y_label = model(x)
-        y = torch.cat([y, y_label])
-    end = time.time()
-        
-    print("Prediction time: {} seconds". format(end - start))
-    
-    return y
+	#We import time to compute time of prediction
+	import time
+	import torch
+
+	x = PPD.collate_data(data)[0]
+	y = torch.tensor([])
+
+	start = time.time()
+	for key, model in models.items():
+		y_label = model(x)
+		y = torch.cat([y, y_label])
+	end = time.time()
+		
+	print("Prediction time: {} seconds". format(end - start))
+
+	return y
 
 def main():
 
@@ -66,7 +66,7 @@ def main():
 	#We load the labels
 	with open('labels.pkl') as f:
 		labels = pickle.load(f)
-		
+	
 	#We set the folder containing the data already prepared for predicting
 	data_folder = 'datasets'
 	data_file = join(data_folder, 'test_dataset_label6.pkl')
